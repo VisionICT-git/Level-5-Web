22e297541a946599accc0d8d3d9c63db2cd23589
VisionICT-git <ICT@visioncollege.ac.nz>
2025-05-01 14:40:22 +1200

    Add files via upload

---
+++ JS/variables/variable.js
@@ -0,0 +1,5 @@
+var dwdd501 = 65;
+var dwdd502 = 65;
+var dwdd503 = 65;
+var total =  dwdd501 + dwdd502+ dwdd503;
+document.getElementById("result").innerHTML = "Total score out of 300 is:" + total;
